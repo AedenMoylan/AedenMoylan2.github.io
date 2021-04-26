329e9f120eeea028ad1eb776274e91c250025650
AedenMoylan <aedenmoylan@gmail.com>
2021-04-26 21:23:12 +0100

    hopeful fix

--- js/script.js
+++ js/script.js
@@ -414,10 +414,10 @@ function animate() {
 
 
   context.drawImage(xSprite1, xSpriteXPos[0] + 30, xSpriteYPos[0] + 30, 42, 42);
-  context.drawImage(xSprite2, xSpriteXPos[1] + 30, xSpriteYPos2[1] + 30, 42, 42);
-  context.drawImage(xSprite3, xSpriteXPos[2] + 30, xSpriteYPos3[2] + 30, 42, 42);
-  context.drawImage(xSprite4, xSpriteXPos[3] + 30, xSpriteYPos4[3] + 30, 42, 42);
-  context.drawImage(xSprite5, xSpriteXPos[4] + 30, xSpriteYPos5[4] + 30, 42, 42);
+  context.drawImage(xSprite2, xSpriteXPos[1] + 30, xSpriteYPos[1] + 30, 42, 42);
+  context.drawImage(xSprite3, xSpriteXPos[2] + 30, xSpriteYPos[2] + 30, 42, 42);
+  context.drawImage(xSprite4, xSpriteXPos[3] + 30, xSpriteYPos[3] + 30, 42, 42);
+  context.drawImage(xSprite5, xSpriteXPos[4] + 30, xSpriteYPos[4] + 30, 42, 42);
 
 
   context.drawImage(explosionSprite, (explosionSprite.width / 4) * currentFrame, (explosionSprite.height / 4), 64, 64, explosionPositionX, explosionPositionY, 100, 100);
