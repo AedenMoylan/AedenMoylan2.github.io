f3798543452da2b35f68d49f4d93dcdd1bc70284
AedenMoylan <aedenmoylan@gmail.com>
2021-04-26 18:04:09 +0100

    added hopeful animations

--- js/script.js
+++ js/script.js
@@ -360,10 +360,12 @@ function animate() {
   }
 
   // Draw sprites
-  //context.drawImage(backGroundSprite, 0, 0, 800, 600);
+  context.drawImage(backGroundSprite, 0, 0, 800, 600);
   context.drawImage(gridSprite, 0, 0, 800, 800);
   context.drawImage(sprite, (0), (0), 42, 42, gameobjects[0].x, gameobjects[0].y, 100, 100);
   context.drawImage(xSprite, xSpriteXPos + 30, xSpriteYPos + 30, 42, 42);
+
+  context.drawImage(splashSprite, (splashSprite.width / 4) * currentFrame, (sprite.height / 2), 62, 33, 200, 200, 100, 100);
 
   // draws gameover is health <= 0
   if (playerHealth <= 0) {
@@ -371,7 +373,6 @@ function animate() {
   }
 
 
-  drawHealthbar();
 }
 
 function onPageLoad() {
