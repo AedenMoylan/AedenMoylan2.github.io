6ea1ae3faca1994e23870a88c60d542f8de48c3e
AedenMoylan <aedenmoylan@gmail.com>
2021-04-26 20:59:48 +0100

    added potential array draw fix

--- js/script.js
+++ js/script.js
@@ -413,7 +413,7 @@ function animate() {
   context.drawImage(splashSprite, (splashSprite.width / 4) * currentFrame, (splashSprite.height / 2), 62, 33, splashPositionX, splashPositionY, 100, 100);
 
   for (i = 0; i < 5; i++) {
-    context.draw(xSprite[i], xSpriteXPos[i] + 30, xSpriteYPos[i] + 30, 42, 42);
+    context.drawImage(xSprite[i], xSpriteXPos[i] + 30, xSpriteYPos[i] + 30, 42, 42);
   }
 
   context.drawImage(explosionSprite, (explosionSprite.width / 4) * currentFrame, (explosionSprite.height / 4), 64, 64, explosionPositionX, explosionPositionY, 100, 100);
